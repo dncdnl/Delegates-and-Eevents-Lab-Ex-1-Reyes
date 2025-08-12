7ea436403a5cb5405b93795218f99008a3613b8d
dncdnl <danicadaniela.cesicar@gmail.com>
2025-08-12 21:38:34 +0800

    Static Return Type Methods

--- Account Registration/StudentInfoClass.cs
+++ Account Registration/StudentInfoClass.cs
@@ -21,5 +21,15 @@ namespace Account_Registration
         public static long Age = 0;
         public static long ContactNo = 0;
         public static long StudentNo = 0;
+        //Return types
+        public static string GetFirstName(string FirstName) =>FirstName;
+        public static string GetLastName(string LastName) => LastName;
+        public static string GetMiddleName(string MiddleName) => MiddleName;
+        public static string GetProgram(string Program) => Program;
+        public static string GetAddress(string Address) => Address;
+        public static long GetAge(long Age) => Age;
+        public static long GetContactNO(long ContactNo) => ContactNo;
+        public static long GetStudentNo(long StudentNo) => StudentNo;
+
     }
 }
